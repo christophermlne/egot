61262681230991348ce0ab088d13050ee9e6fec6
Christopher Milne <christopher.milne@gmail.com>
2026-01-10 23:07:05 -0500

    [5] Add RequireMC plug and seed MC user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Egot.Repo
+alias Egot.Accounts.User
+
+# Create MC user if it doesn't exist
+mc_email = "mc@example.com"
+
+unless Repo.get_by(User, email: mc_email) do
+  %User{}
+  |> Ecto.Changeset.change(%{
+    email: mc_email,
+    hashed_password: Bcrypt.hash_pwd_salt("password123"),
+    is_mc: true,
+    confirmed_at: DateTime.utc_now(:second)
+  })
+  |> Repo.insert!()
+
+  IO.puts("Created MC user: #{mc_email} / password123")
+end
